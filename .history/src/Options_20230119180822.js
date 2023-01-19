b80a7076db5d33e78866705cd962fabba1f26fef
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2023-01-19 18:24:42 +0000

    26rem media query

---
+++ .history/src/Options_20230119180822.js
@@ -0,0 +1,18 @@
+import styles from "../src/Options.module.scss";
+
+const Options = (props) => {
+	return (
+		<>
+			<div>
+				<button className={styles.button1} onClick={props.onClick}>
+					<span></span>
+					<span></span>
+					<span></span>
+					<span></span> Choose a category
+				</button>
+		
+		</>
+	);
+};
+
+export default Options;
