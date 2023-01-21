b1d09a48b90bfcd692cc4e3948524fc355256cbe
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2023-01-21 13:47:32 +0000

    media queries updated all devices2

---
+++ .history/src/Components/Phrase_20230121132854.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Loading } from "./Loading";
+import styles from "./Phrase.module.scss";
+
+function Phrase(props) {
+	const { data, handleNext } = props;
+	return (
+		<section className={styles.um}>
+			<div>
+				{data && <img src={data.image_url} alt="image_phrase" />}
+				{data ? <h1>{data.phrase}</h1> : <Loading />}
+			</div>
+			<button className={styles.next} onClick={handleNext}>
+				Next
+			</button>
+			<Tit
+		</section>
+	);
+}
+
+export { Phrase };
---
+++ .history/src/Components/Phrase_20230121132858.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Loading } from "./Loading";
+import styles from "./Phrase.module.scss";
+import TitleDown from "./TitleDown";
+
+function Phrase(props) {
+	const { data, handleNext } = props;
+	return (
+		<section className={styles.um}>
+			<div>
+				{data && <img src={data.image_url} alt="image_phrase" />}
+				{data ? <h1>{data.phrase}</h1> : <Loading />}
+			</div>
+			<button className={styles.next} onClick={handleNext}>
+				Next
+			</button>
+			<TitleDown?>
+		</section>
+	);
+}
+
+export { Phrase };
